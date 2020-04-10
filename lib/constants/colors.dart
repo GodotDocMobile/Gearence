bf874c77a7c8622c996c86d5af036e331f9b7c37
fengjiongmax <fengjiongmax@gmail.com>
2020-04-10 13:07:59 +0800

    initial committ

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+Map<int, Color> godotThemeColorMap = {
+  50: Color.fromRGBO(101, 137, 189, .1),
+  100: Color.fromRGBO(101, 137, 189, .2),
+  200: Color.fromRGBO(101, 137, 189, .3),
+  300: Color.fromRGBO(101, 137, 189, .4),
+  400: Color.fromRGBO(101, 137, 189, .5),
+  500: Color.fromRGBO(101, 137, 189, .6),
+  600: Color.fromRGBO(101, 137, 189, .7),
+  700: Color.fromRGBO(101, 137, 189, .8),
+  800: Color.fromRGBO(101, 137, 189, .9),
+  900: Color.fromRGBO(101, 137, 189, 1),
+};
