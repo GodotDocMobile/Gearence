af6ff9c64a35be3491317c10030ddeaa06f01eb0
fengjiongmax <fengjiongmax@gmail.com>
2020-04-18 00:09:24 +0800

    trim xml files with lxml

--- scripts/generate_file_index.py
+++ scripts/generate_file_index.py
@@ -2,17 +2,14 @@
 import json
 from os import listdir
 from os.path import isfile,join
+# import xml.etree.ElementTree as ET
+from lxml import etree
 
 def strip_file(file_path):
     print(file_path)
-    f = open(file_path,"r")
-    content = f.read()
-    f.close()
-
-    f = open(file_path,"w")
-    f.write(content.replace('\n','').replace('\t',''))
-    f.close()
-    pass
+    parser = etree.XMLParser(remove_blank_text=True)
+    tree = etree.parse(file_path,parser)
+    tree.write(file_path,pretty_print=False)
 
 if __name__ == "__main__":
     onlyfiles = [f for f in listdir("../xmls/3.0") if isfile(join("../xmls/3.0/",f))]
@@ -21,8 +18,8 @@ if __name__ == "__main__":
             print('parsing '+ver+' ...')
             files = [f for f in listdir(join("../xmls",ver)) if isfile(join("../xmls",ver,f))]
             files.sort()
-            # for json_file in files:
-            #     strip_file(join("../xmls",ver,json_file))
+            for json_file in files:
+            	strip_file(join("../xmls",ver,json_file))
             files_json = json.dumps(files)
             f = open(join("../xmls","files_"+ver+".json"),"w")
             f.write(files_json)
