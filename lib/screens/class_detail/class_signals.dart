332c644097d0cede48c8a8e8805e40ae2e72606b
fengjiongmax <fengjiongmax@gmail.com>
2020-04-25 10:11:29 +0800

    switch from listview to ItemPositionedList

--- lib/screens/class_detail/class_signals.dart
+++ lib/screens/class_detail/class_signals.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:godotclassreference/components/description_text.dart';
+import 'package:godotclassreference/constants/tap_event_arg.dart';
 import 'package:godotclassreference/models/class_content.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class ClassSignals extends StatelessWidget {
   final ClassContent clsContent;
+  final Function(TapEventArg args) onLinkTap;
 
-  ClassSignals({Key key, this.clsContent}) : super(key: key);
+  final ItemScrollController _scrollController = ItemScrollController();
+  final ItemPositionsListener _itemPositionsListener =
+      ItemPositionsListener.create();
+
+  ClassSignals({Key key, this.clsContent, @required this.onLinkTap})
+      : assert(onLinkTap != null),
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,23 +25,25 @@ class ClassSignals extends StatelessWidget {
       );
     }
 
-    return ListView(
-      padding: EdgeInsets.all(5),
-      children: clsContent.signals.map((s) {
-        return ListTile(
-          title: Text(
-            s.name +
-                s.arguments.map((a) {
-                  return a.type + " " + a.name;
-                }).toString(),
-          ),
-          subtitle: DescriptionText(
-            className: clsContent.name,
-            content: s.description,
-            onLinkTap: (e) {},
-          ),
-        );
-      }).toList(),
-    );
+    return ScrollablePositionedList.builder(
+        itemCount: clsContent.signals.length,
+        itemScrollController: _scrollController,
+        itemPositionsListener: _itemPositionsListener,
+        itemBuilder: (context, index) {
+          final s = clsContent.signals[index];
+          return ListTile(
+            title: Text(
+              s.name +
+                  s.arguments.map((a) {
+                    return a.type + " " + a.name;
+                  }).toString(),
+            ),
+            subtitle: DescriptionText(
+              className: clsContent.name,
+              content: s.description,
+              onLinkTap: onLinkTap,
+            ),
+          );
+        });
   }
 }
