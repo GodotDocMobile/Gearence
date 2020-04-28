ae72fd093c5a3e66b5fe8b0f354d53324b647c09
fengjiongmax <fengjiongmax@gmail.com>
2020-04-28 16:01:48 +0800

    class detail tab switching for tappable tags rendered by description_text

--- lib/screens/class_detail.dart
+++ lib/screens/class_detail.dart
@@ -15,19 +15,42 @@ import 'package:godotclassreference/screens/class_detail/class_theme_items.dart'
 
 import 'package:godotclassreference/models/class_content.dart';
 
-class ClassDetail extends StatelessWidget {
+class ClassDetail extends StatefulWidget {
   final String className;
+  final TapEventArg args;
 
-  const ClassDetail({Key key, @required this.className})
+  const ClassDetail({Key key, @required this.className, this.args})
       : assert(className != null),
         super(key: key);
+
+  @override
+  _ClassDetailState createState() => _ClassDetailState();
+}
+
+class _ClassDetailState extends State<ClassDetail>
+    with TickerProviderStateMixin {
+  TabController _tabController;
+  Future<ClassContent> _classContent;
+  List<ClassTab> _tabs;
+
+  @override
+  void initState() {
+    super.initState();
+    _classContent = getClassDetail();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   Future<ClassContent> getClassDetail() async {
     final version = StoredValues().prefs.getString('version');
 
     final file = await rootBundle
-        .loadString('xmls/' + version + '/' + className + '.xml');
-//    final aaa = xml.parse(file).root.children;
+        .loadString('xmls/' + version + '/' + widget.className + '.xml');
+
     final rootNode = xml
         .parse(file)
         .root
@@ -36,21 +59,30 @@ class ClassDetail extends StatelessWidget {
     return ClassContent.fromXml(rootNode);
   }
 
-  void onLinkTap(TapEventArg args) {}
+  void onLinkTap(TapEventArg args) {
+    if (args.className == widget.className) {
+      //navigation within class
+      int _toFocusTabIndex =
+          _tabs.indexWhere((w) => w.title == linkTypeToString(args.linkType));
+      _tabController.animateTo(_toFocusTabIndex);
+    } else {
+//      Navigator
+    }
+    print(
+        args.className + ":" + args.linkType.toString() + ":" + args.fieldName);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final _classContent = getClassDetail();
-
     return FutureBuilder<ClassContent>(
       future: _classContent,
       builder: (BuildContext context, AsyncSnapshot<ClassContent> snapshot) {
         if (snapshot.hasData) {
-          List<ClassTab> tabs = getClassTabs(snapshot.data, this.onLinkTap);
+          _tabs = getClassTabs(snapshot.data, this.onLinkTap);
           // append theme item tab if needed
           if (snapshot.data.themeItems != null &&
               snapshot.data.themeItems.length > 0) {
-            tabs.add(ClassTab(
+            _tabs.add(ClassTab(
               title: 'Theme Item',
               child: ClassThemeItems(
                 clsContent: snapshot.data,
@@ -60,58 +92,63 @@ class ClassDetail extends StatelessWidget {
               itemCount: snapshot.data.themeItems.length,
             ));
           }
-          return DefaultTabController(
-            length: tabs.length,
-            child: Scaffold(
-              appBar: AppBar(
-                title: Text(className),
-                bottom: TabBar(
-                  isScrollable: true,
-                  tabs: tabs.map((f) {
-                    return Tab(
-                      child: Row(
-                        children: <Widget>[
-                          Text(f.title),
-                          f.showCnt
-                              ? Row(
-                                  children: <Widget>[
-                                    SizedBox(
-                                      width: 5,
+
+          _tabController = TabController(vsync: this, length: _tabs.length);
+
+//          _tabController.animateTo(1);
+
+          if (widget.args != null &&
+              widget.args.className == snapshot.data.name) {
+            int _tabIndex = _tabs.indexWhere(
+                (w) => w.title == linkTypeToString(LinkType.Constant));
+            _tabController.animateTo(_tabIndex);
+          }
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(widget.className),
+              bottom: TabBar(
+                controller: _tabController,
+                isScrollable: true,
+                tabs: _tabs.map((f) {
+                  return Tab(
+                    child: Row(
+                      children: <Widget>[
+                        Text(f.title),
+                        f.showCnt
+                            ? Row(
+                                children: <Widget>[
+                                  SizedBox(
+                                    width: 5,
+                                  ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(3)),
                                     ),
-                                    Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(3)),
-                                      ),
-                                      height: 20,
-                                      child: Center(
-                                        child: Text(
-                                          " " + f.itemCount.toString() + " ",
-                                          style: TextStyle(color: Colors.black),
-                                        ),
+                                    height: 20,
+                                    child: Center(
+                                      child: Text(
+                                        " " + f.itemCount.toString() + " ",
+                                        style: TextStyle(color: Colors.black),
                                       ),
                                     ),
-                                  ],
-                                )
-                              : SizedBox()
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ),
-              body: TabBarView(
-                children: tabs.map((c) {
-                  return c.child;
+                                  ),
+                                ],
+                              )
+                            : SizedBox()
+                      ],
+                    ),
+                  );
                 }).toList(),
               ),
-//              bottomNavigationBar: Row(
-//                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                children: <Widget>[
-//                  Text('Hello'),
-//                ],
-//              ),
+            ),
+            body: TabBarView(
+              controller: _tabController,
+              children: _tabs.map((c) {
+                return c.child;
+              }).toList(),
             ),
           );
         }
