09551b37f4a846b986c5850105c58e2b4c628519
fengjiongmax <fengjiongmax@gmail.com>
2020-05-02 12:51:10 +0800

    properties in stored_values.dart that is for resume last read

--- lib/constants/stored_values.dart
+++ lib/constants/stored_values.dart
@@ -5,6 +5,10 @@ class StoredValues {
 
   static final StoredValues _instance = StoredValues._internal();
 
+  static final List<int> scrollIndexes = List<int>();
+  static final List<int> tabIndexes = List<int>();
+  static final List<String> classes = List<String>();
+
   factory StoredValues() {
     if (_instance.prefs == null) {
       _instance.readValue();
@@ -17,5 +21,20 @@ class StoredValues {
 
   Future<void> readValue() async {
     prefs = await SharedPreferences.getInstance();
+
+    int _scrollIndex = prefs.getInt('scrollIndex');
+    if (_scrollIndex != null) {
+      scrollIndexes.add(_scrollIndex);
+    }
+
+    int _tabIndex = prefs.getInt('tabIndex');
+    if (_tabIndex != null) {
+      scrollIndexes.add(_tabIndex);
+    }
+
+    String _className = prefs.getString('className');
+    if (_className != null && _className.length > 0) {
+      classes.add(_className);
+    }
   }
 }
