f440617c12593de09165ae1fdcae465be8d7b3dd
fengjiongmax <fengjiongmax@gmail.com>
2021-02-11 02:30:13 +0800

    improve code deisplay

---
+++ lib/components/inline_code.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class InlineCode extends StatelessWidget {
+  final String codeText;
+
+  const InlineCode({Key key, this.codeText}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 2, right: 2),
+      decoration: BoxDecoration(
+        color: Colors.black12,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Text(codeText),
+    );
+  }
+}
