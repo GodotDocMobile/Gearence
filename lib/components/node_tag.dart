3dd6d2a8b4050ed22558cbc84f4757fab771e6a1
fengjiongmax <fengjiongmax@gmail.com>
2021-11-07 19:01:55 +0800

    *hopefully* simplify class filter mechanic

--- lib/components/node_tag.dart
+++ lib/components/node_tag.dart
@@ -9,29 +9,16 @@ class NodeTag extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool visualScript = false;
-    Color _backColor = Colors.blueGrey;
-
-    if (classContent.belong('Node2D')) {
-      _backColor = Color(0xffa5b7f3);
-    } else if (classContent.belong('Spatial')) {
-      _backColor = Color(0xfffc9c9c);
-    } else if (classContent.belong('Control')) {
-      _backColor = Color(0xffa5efac);
-    } else if (classContent.belong('VisualScriptNode')) {
-      visualScript = true;
-    }
-
     return Container(
       decoration: BoxDecoration(
-        color: _backColor,
+        color: tagColor[classContent.nodeType],
         borderRadius: BorderRadius.circular(5),
       ),
       width: 45,
       height: 20,
       child: Center(
         child: Text(
-          visualScript ? "Visual" : "Node",
+          tagName[classContent.nodeType],
           style: TextStyle(color: Colors.white, fontSize: 15),
         ),
       ),
