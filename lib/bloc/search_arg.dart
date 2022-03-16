511d467b6c491f33de4ca314ac1a94ef7ea66387
fengjiongmax <fengjiongmax@gmail.com>
2022-03-16 08:07:15 +0800

    optimize search result displaying #8

---
+++ lib/bloc/search_arg.dart
@@ -0,0 +1,8 @@
+import 'tap_event_arg.dart';
+
+class SearchEventArg {
+  final TapEventArg tapEventArg;
+  final double rank;
+
+  SearchEventArg({required this.tapEventArg, required this.rank});
+}
