574c95ddfa6241a9c8f46c73a57dd1ac63dd97db
fengjiongmax <fengjiongmax@gmail.com>
2022-03-16 08:07:15 +0800

    optimize search result displaying #8

--- lib/bloc/search_bloc.dart
+++ lib/bloc/search_bloc.dart
@@ -1,13 +1,17 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:godotclassreference/bloc/search_arg.dart';
 
 import 'tap_event_arg.dart';
 
 abstract class SearchEvent {}
 
-class SearchBloc extends Cubit<TapEventArg> {
-  SearchBloc() : super(TapEventArg(className: '-', fieldName: '-'));
+class SearchBloc extends Cubit<SearchEventArg> {
+  SearchBloc()
+      : super(SearchEventArg(
+            tapEventArg: TapEventArg(className: '-', fieldName: '-'),
+            rank: 10));
 
-  void add(TapEventArg eventArg) {
+  void add(SearchEventArg eventArg) {
     emit(eventArg);
   }
 }
