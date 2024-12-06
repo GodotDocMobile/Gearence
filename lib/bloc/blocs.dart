c04c41690e7a26b12534faffe2c391c456da1074
fengjiongmax <fengjiongmax@gmail.com>
2024-12-06 20:20:38 +0800

    switch language, but throw error on ga v4.3

--- lib/bloc/blocs.dart
+++ lib/bloc/blocs.dart
@@ -2,6 +2,7 @@ import 'package:godotclassreference/bloc/monospace_font_bloc.dart';
 import 'package:godotclassreference/bloc/tap_event_bloc.dart';
 import 'package:godotclassreference/bloc/theme_bloc.dart';
 import 'package:godotclassreference/bloc/translation_bloc.dart';
+import 'package:godotclassreference/bloc/version_bloc.dart';
 import 'package:godotclassreference/constants/stored_values.dart';
 
 export 'xml_load_bloc.dart';
@@ -11,6 +12,7 @@ export 'theme_bloc.dart';
 export 'tap_event_bloc.dart';
 export 'tap_event_arg.dart';
 export 'translation_bloc.dart';
+export 'version_bloc.dart';
 
 class AllBlocs {
   MonospaceFontBloc monospaceFontBloc =
@@ -18,6 +20,7 @@ class AllBlocs {
   ThemeChangeBloc themeChangeBloc = ThemeChangeBloc(storedValues.isDarkTheme);
   TapEventBloc tapEventBloc = TapEventBloc();
   TranslationBloc translationBloc = TranslationBloc('');
+  VersionBloc versionBloc = VersionBloc('');
 
   static final AllBlocs _singleton = AllBlocs._internal();
 
